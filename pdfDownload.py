8903cf4f4d621910f6ec306eb9c07c5fd4a48450
chipdelmal <chipdelmal@gmail.com>
2020-09-13 15:37:10 -0700

    Took a shot at Selenium (could be useful but delete)

--- pdfDownload.py
+++ pdfDownload.py
@@ -2,12 +2,25 @@
 
 import requests
 from datetime import date
+from selenium import webdriver
 
 
 nid = 'ZIKV'
 url = 'https://www.cdph.ca.gov/Programs/CID/DCDC/CDPH%20Document%20Library/'
 urlPdf = url + 'TravelAssociatedCasesofZikaVirusinCA.pdf'
 out = './docs/'
+DRV = './chromedriver/chromedriver'
+
+
+prefs = {
+        "download.default_directory": out,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "plugins.plugins_disabled": ["Chrome PDF Viewer"],
+        "plugins.always_open_pdf_externally": True
+    }
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(executable_path=DRV, chrome_options=options)
 # Request the PDF from url
 response = requests.get(url)
 # Write to disk
